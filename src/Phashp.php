61f4f05d1969c908bbd6098bdee8046224890ee9
Roger <rogervila@me.com>
2017-01-11 20:48:03 +0100

    doc blocs

--- src/Phashp.php
+++ src/Phashp.php
@@ -9,30 +9,75 @@ class Phashp
 {
     use DefaultAlgorithms, Hasher;
 
+    /**
+     * @var Phashp
+     */
     protected static $instance;
+
+    /**
+     * @var array
+     */
     protected $defaultAlgorithms;
+
+    /**
+     * @var array
+     */
     protected $algorithms;
+
+    /**
+     * @var string
+     */
     protected $outputAlgorithm;
+
+    /**
+     * @var string
+     */
     protected $string;
+
+    /**
+     * @var int
+     */
     protected $counter;
+
+    /**
+     * @var string
+     */
     protected $hashedString;
 
+    /**
+     * Phashp constructor.
+     */
     public function __construct()
     {
         $this->algorithms = $this->getDefaultAlgorithms();
         $this->counter = 1;
     }
 
+    /**
+     * @param $name
+     * @param $arguments
+     * @return Phashp
+     */
     public static function __callStatic($name, $arguments)
     {
         return self::handle($name, $arguments);
     }
 
+    /**
+     * @param $name
+     * @param $arguments
+     * @return Phashp
+     */
     public function __call($name, $arguments)
     {
         return self::handle($name, $arguments);
     }
 
+    /**
+     * @param $name
+     * @param $arguments
+     * @return Phashp
+     */
     protected static function handle($name, $arguments)
     {
         if (is_null(self::$instance)) {
@@ -49,6 +94,11 @@ class Phashp
         return self::$instance;
     }
 
+    /**
+     * @param $algorithms
+     * @return $this
+     * @throws \Exception
+     */
     protected function parseAlgos($algorithms)
     {
         if ( ! is_array($algorithms)) {
@@ -60,6 +110,11 @@ class Phashp
         return $this;
     }
 
+    /**
+     * @param $integer
+     * @return $this
+     * @throws \Exception
+     */
     protected function parseCycles($integer)
     {
         if ( ! is_int($integer)) {
@@ -75,6 +130,11 @@ class Phashp
         return $this;
     }
 
+    /**
+     * @param $algorithm
+     * @return $this
+     * @throws \Exception
+     */
     protected function parseOutput($algorithm)
     {
         if ( ! in_array($algorithm, hash_algos())) {
@@ -86,6 +146,11 @@ class Phashp
         return $this;
     }
 
+    /**
+     * @param $string
+     * @return string
+     * @throws \Exception
+     */
     protected function parseHash($string)
     {
         if ( ! is_string($string)) {
